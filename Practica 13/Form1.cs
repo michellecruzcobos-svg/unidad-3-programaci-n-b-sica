b698cff47e838d55ab1bd86f511839e37b0b7d88
michellecruzcobos-svg <michelle.cruzcobos@gmail.com>
2026-03-18 11:40:53 -0500

    Add files via upload

---
+++ Practica 13/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sueldo, aum, nsue;
+            sueldo = double.Parse(textBox1.Text);
+            if (sueldo < 400000.00)
+            {
+                aum = sueldo * 0.15;
+                nsue = sueldo + aum;
+                textBox2.Text = nsue.ToString();
+            }
+        }
+    }
+}
