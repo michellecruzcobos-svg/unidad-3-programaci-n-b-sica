b5a2712f7979252ad29a0963a671e3b092f2ea5a
michellecruzcobos-svg <michelle.cruzcobos@gmail.com>
2026-03-18 11:34:02 -0500

    Add files via upload

---
+++ Hello_Mundo/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello_Mundo
+{
+    public partial class Hello_Mundo : Form
+    {
+        public Hello_Mundo()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Hello, " + textBox1.Text + "!";
+        }
+    }
+}
---
+++ Hello_Mundo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello_Mundo
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Hello_Mundo());
+        }
+    }
+}
