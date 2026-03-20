8e085dafcb36b00bbfc3bfa671cd11ee754bca18
michellecruzcobos-svg <michelle.cruzcobos@gmail.com>
2026-03-20 18:00:57 -0500

    Add files via upload

---
+++ WhileDo6/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhileDo6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num;
+
+            if (int.TryParse(textBox1.Text, out num))
+            {
+                if (num % 2 == 0)
+                {
+                    listBox1.Items.Add(num + " Par");
+                }
+                else
+                {
+                    listBox1.Items.Add(num + " Impar");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingresa un número válido");
+            }
+
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+    }
+}
